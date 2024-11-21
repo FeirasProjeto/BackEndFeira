d5daee44eab7455489c27341c0758cba6e33990e
diego <feirass.projeto@gmail.com>
2024-11-21 09:59:15 -0300

    🐛fix: cors issues

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import path from 'path'
+import cors from 'cors'
 
 const caminho = path.resolve(__dirname, 'swaggerDoc.json')
 
@@ -11,31 +12,23 @@ const app = express()
 // const port = 3000
 const port = process.env.PORT ?? 3000
 
-// Adiciona titulo e icone
-// const uiOptions = {
-//   customSiteTitle: 'Minha Feira',
-//   customfavIcon: 'https://imgur.com/a/SuX7ynl'
-// }
-
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
-
 
 import userRoutes from './routes/user'
 import feiraRoutes from './routes/feira'
 import favoritoRoutes from './routes/favorito'
-import swaggerJSDoc from 'swagger-jsdoc'
 
 app.use(express.json())
+app.use(cors())
+
 app.use("/usuario", userRoutes)
 app.use("/feira", feiraRoutes)
 app.use("/favorito", favoritoRoutes)
 
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc, {customCssUrl: CSS_URL}))
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc, {customCssUrl: CSS_URL, }))
 
 // app.get('/', swaggerUi.setup(swaggerDoc, uiOptions))
-
-
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: http://localhost:${port}`)
