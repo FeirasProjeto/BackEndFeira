0277eab67e6f4413c21641dec3733b60c9520417
diego <feirass.projeto@gmail.com>
2024-11-30 16:54:11 -0300

    📝 feat: update swagger docs

--- routes/tag.ts
+++ routes/tag.ts
@@ -36,10 +36,14 @@ router.post("/", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const { id } = req.params
 
+  try {
   const tag = await prisma.tag.delete({
     where: { id: id }
   })
   res.status(200).json(tag)
+  } catch (error) {
+    res.status(400).json({ error: "Id invalido" })
+  }
 })
 
 
