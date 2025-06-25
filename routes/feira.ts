7af489f646915869d3dec6fe0b55834cefbf1ab4
Debora Carvalho <dbndy3@gmail.com>
2025-06-25 10:48:04 -0300

    📃refactor: melhorar formatação e estrutura do schema de feira

--- routes/feira.ts
+++ routes/feira.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
-import {z} from "zod";
+import { z } from "zod";
 
 const prisma = new PrismaClient();
 const router = Router();
@@ -15,40 +15,60 @@ const feiraSchema = z.object({
   horarioFim: z.string().min(1, "Horário de fim é obrigatório"),
   descricao: z.string().optional(),
   imagem: z.string().optional(),
-  tags: z.array(z.object({
-    id: z.string().uuid(),
-  })).optional(),
-  diaSemana: z.array(z.object({
-    nome: z.string().min(1, "Dia da semana é obrigatório"),
-  })).optional(),  
-  Avaliacoes: z.array(z.object({
-    id: z.string().uuid(),
-    nota: z.number().min(1, "Nota deve ser maior ou igual a 1").max(5, "Nota deve ser menor ou igual a 5"),
-    comentario: z.string().optional(),
-    user: z.object({
-      id: z.string().uuid(),
-      nome: z.string().min(1, "Nome do usuário é obrigatório"),
-      imagem: z.string().optional()
-    }).optional()
-  })).optional(),
+  tags: z
+    .array(
+      z.object({
+        id: z.string().uuid(),
+      })
+    )
+    .optional(),
+  diaSemana: z
+    .array(
+      z.object({
+        nome: z.string().min(1, "Dia da semana é obrigatório"),
+      })
+    )
+    .optional(),
+  Avaliacoes: z
+    .array(
+      z.object({
+        id: z.string().uuid(),
+        nota: z
+          .number()
+          .min(1, "Nota deve ser maior ou igual a 1")
+          .max(5, "Nota deve ser menor ou igual a 5"),
+        comentario: z.string().optional(),
+        user: z
+          .object({
+            id: z.string().uuid(),
+            nome: z.string().min(1, "Nome do usuário é obrigatório"),
+            imagem: z.string().optional(),
+          })
+          .optional(),
+      })
+    )
+    .optional(),
   userId: z.string().uuid().min(1, "ID do usuário é obrigatório"),
   categoria: z.object({
     id: z.string().uuid(),
     nome: z.string().min(1, "Nome da categoria é obrigatório"),
     cor: z.string().min(1, "Cor da categoria é obrigatória"),
   }),
-  favoritos: z.array(z.object({
-    id: z.string().uuid(),
-    user: z.object({
+  favoritos: z.array(
+    z.object({
       id: z.string().uuid(),
-      nome: z.string().min(1, "Nome do usuário é obrigatório"),
-      imagem: z.string().optional()
-    }).optional()
-  }))
-
+      user: z
+        .object({
+          id: z.string().uuid(),
+          nome: z.string().min(1, "Nome do usuário é obrigatório"),
+          imagem: z.string().optional(),
+        })
+        .optional(),
+    })
+  ),
 });
 async function main() {
-  //SOFT DELETE MIDDLEWARE 
+  //SOFT DELETE MIDDLEWARE
   prisma.$use(async (params, next) => {
     // Check incoming query type
     if (params.model == "Feira") {
@@ -67,140 +87,29 @@ main();
 // CRUD
 // Read
 router.get("/", async (req, res) => {
-  const feiras = await prisma.feira.findMany(
-    {
-      where: { deleted: false },
-      include: {
-        _count: {
-          select: {
-            favoritos: true,
-            Avaliacoes: true
-          }
-        },
-        tags: {
-          select: {
-            tag: {
-              select: {
-                id: true,
-                nome: true
-              }
-            }
-          }
-        },
-        diaSemana: {
-          select: {
-            diaSemana: true
-          }
-        },
-        Avaliacoes: {
-          select: {
-            id: true,
-            nota: true,
-            comentario: true,
-            user: {
-              select: {
-                id: true,
-                nome: true,
-                imagem: true
-              }
-            }
-          }
-        }
-      }
-    }
-  );
-
-  res.status(200).json(feiras);
-});
-
-
-// filtros
-router.get("/filtros", async (req, res) => {
-  const { tags, diaSemana, horario, pesquisa } = req.query;
-
-  const filters: any = {
-    deleted: false,
-    nome: {
-      contains: pesquisa as string,
-      mode: "insensitive"
-    }
-  };
-  if (tags) {
-    const tagNomes = (tags as string).split(",");
-
-    const tagIds = await prisma.tag.findMany({
-      where: {
-        nome: {
-          in: tagNomes
-        }
-      },
-      select: { id: true }
-    });
-
-    const ids = tagIds.map(tag => tag.id);
-
-    filters.tags = {
-      some: {
-        tagId: {
-          in: ids
-        }
-      }
-    };
-  }
-
-  if (diaSemana) {
-    const nomesDias = (diaSemana as string).split(",");
-
-    const dias = await prisma.diaSemana.findMany({
-      where: {
-        nome: {
-          in: nomesDias
-        }
-      },
-      select: { id: true }
-    });
-
-    const ids = dias.map(dia => dia.id);
-
-    filters.diaSemana = {
-      some: {
-        diaSemanaId: {
-          in: ids
-        }
-      }
-    };
-  }
-
-  if (horario) {
-    filters.horario = {
-      contains: horario as string,
-      mode: "insensitive"
-    };
-  }
-
   const feiras = await prisma.feira.findMany({
-    where: filters,
+    where: { deleted: false },
     include: {
       _count: {
         select: {
           favoritos: true,
-          Avaliacoes: true
-        }
+          Avaliacoes: true,
+        },
       },
       tags: {
         select: {
           tag: {
             select: {
               id: true,
-              nome: true
-            }
-          }
-        }
+              nome: true,
+            },
+          },
+        },
       },
       diaSemana: {
         select: {
-          diaSemana: true
-        }
+          diaSemana: true,
+        },
       },
       Avaliacoes: {
         select: {
@@ -211,12 +120,120 @@ router.get("/filtros", async (req, res) => {
             select: {
               id: true,
               nome: true,
-              imagem: true
-            }
-          }
-        }
-      }
-    }
+              imagem: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  res.status(200).json(feiras);
+});
+
+// filtros
+router.get("/filtros", async (req, res) => {
+  const { tags, diaSemana, horario, pesquisa } = req.query;
+
+  const filters: any = {
+    deleted: false,
+    nome: {
+      contains: pesquisa as string,
+      mode: "insensitive",
+    },
+  };
+  if (tags) {
+    const tagNomes = (tags as string).split(",");
+
+    const tagIds = await prisma.tag.findMany({
+      where: {
+        nome: {
+          in: tagNomes,
+        },
+      },
+      select: { id: true },
+    });
+
+    const ids = tagIds.map((tag) => tag.id);
+
+    filters.tags = {
+      some: {
+        tagId: {
+          in: ids,
+        },
+      },
+    };
+  }
+
+  if (diaSemana) {
+    const nomesDias = (diaSemana as string).split(",");
+
+    const dias = await prisma.diaSemana.findMany({
+      where: {
+        nome: {
+          in: nomesDias,
+        },
+      },
+      select: { id: true },
+    });
+
+    const ids = dias.map((dia) => dia.id);
+
+    filters.diaSemana = {
+      some: {
+        diaSemanaId: {
+          in: ids,
+        },
+      },
+    };
+  }
+
+  if (horario) {
+    filters.horario = {
+      contains: horario as string,
+      mode: "insensitive",
+    };
+  }
+
+  const feiras = await prisma.feira.findMany({
+    where: filters,
+    include: {
+      _count: {
+        select: {
+          favoritos: true,
+          Avaliacoes: true,
+        },
+      },
+      tags: {
+        select: {
+          tag: {
+            select: {
+              id: true,
+              nome: true,
+            },
+          },
+        },
+      },
+      diaSemana: {
+        select: {
+          diaSemana: true,
+        },
+      },
+      Avaliacoes: {
+        select: {
+          id: true,
+          nota: true,
+          comentario: true,
+          user: {
+            select: {
+              id: true,
+              nome: true,
+              imagem: true,
+            },
+          },
+        },
+      },
+    },
   });
   if (feiras.length > 0) {
     res.status(200).json(feiras);
@@ -244,23 +261,23 @@ router.get("/:id", async (req, res) => {
       _count: {
         select: {
           favoritos: true,
-          Avaliacoes: true
-        }
+          Avaliacoes: true,
+        },
       },
       tags: {
         select: {
           tag: {
             select: {
               id: true,
-              nome: true
-            }
-          }
-        }
+              nome: true,
+            },
+          },
+        },
       },
       diaSemana: {
         select: {
-          diaSemana: true
-        }
+          diaSemana: true,
+        },
       },
       Avaliacoes: {
         select: {
@@ -271,51 +288,108 @@ router.get("/:id", async (req, res) => {
             select: {
               id: true,
               nome: true,
-              imagem: true
-            }
-          }
-        }
-      }
-    }
+              imagem: true,
+            },
+          },
+        },
+      },
+    },
   });
   res.status(200).json(feira);
 });
 
 // Create
 router.post("/", async (req, res) => {
-  const { nome, endereco, numero, cidade, coordenada, horarioInicio, horarioFim, data, descricao, imagem, tags, diaSemana, userId, categoria } = req.body;
+  const {
+    nome,
+    endereco,
+    numero,
+    cidade,
+    coordenada,
+    horarioInicio,
+    horarioFim,
+    data,
+    descricao,
+    imagem,
+    tags,
+    diaSemana,
+    userId,
+    categoria,
+  } = req.body;
 
-  if (!nome || !endereco || !numero || !cidade || !coordenada || !horarioInicio || !horarioFim || !userId || !categoria) {
-    return res.status(400).json({ message: "Todos os campos devem ser preenchidos", campoFaltante: !nome ? "nome" : !endereco ? "endereco" : !numero ? "numero" : !cidade ? "cidade" : !coordenada ? "coordenada" : !horarioInicio ? "horarioInicio" : !horarioFim ? "horarioFim" : !userId ? "userId" : "" });
+  if (
+    !nome ||
+    !endereco ||
+    !numero ||
+    !cidade ||
+    !coordenada ||
+    !horarioInicio ||
+    !horarioFim ||
+    !userId ||
+    !categoria
+  ) {
+    return res
+      .status(400)
+      .json({
+        message: "Todos os campos devem ser preenchidos",
+        campoFaltante: !nome
+          ? "nome"
+          : !endereco
+          ? "endereco"
+          : !numero
+          ? "numero"
+          : !cidade
+          ? "cidade"
+          : !coordenada
+          ? "coordenada"
+          : !horarioInicio
+          ? "horarioInicio"
+          : !horarioFim
+          ? "horarioFim"
+          : !userId
+          ? "userId"
+          : "",
+      });
   }
 
   let turno = "";
   try {
-  if (horarioInicio && horarioFim) {
+    if (horarioInicio && horarioFim) {
+      const [hora] = horarioInicio.split(":").map(Number);
+      const [horaFim] = horarioFim.split(":").map(Number);
 
-    const [hora] = horarioInicio.split(":").map(Number);
-    const [horaFim] = horarioFim.split(":").map(Number);
-
-    if (hora < 12 && horaFim < 12) {
-      turno = "Manhã";
-    } else if (hora >= 12 && horaFim < 18) {
-      turno = "Tarde";
-    } else if (hora < 12 && horaFim <= 18) {
-      turno = "Manhã e Tarde";
-    } else if (hora >= 18 && horaFim >= 18) {
-      turno = "Noite";
-    } else if (hora >= 12 && horaFim >= 18) {
-      turno = "Tarde e Noite";
-    } else {
-      turno = "Dia inteiro";
+      if (hora < 12 && horaFim < 12) {
+        turno = "Manhã";
+      } else if (hora >= 12 && horaFim < 18) {
+        turno = "Tarde";
+      } else if (hora < 12 && horaFim <= 18) {
+        turno = "Manhã e Tarde";
+      } else if (hora >= 18 && horaFim >= 18) {
+        turno = "Noite";
+      } else if (hora >= 12 && horaFim >= 18) {
+        turno = "Tarde e Noite";
+      } else {
+        turno = "Dia inteiro";
+      }
     }
+  } catch (error) {
+    return res.status(400).json({ message: "Horário inválido" });
   }
-} catch (error) {
-  return res.status(400).json({ message: "Horário inválido" });
-}
 
   const feira = await prisma.feira.create({
-    data: { nome, endereco, numero, cidade, coordenada, horarioInicio, horarioFim, descricao, imagem, userId, turno },
+    data: {
+      nome,
+      endereco,
+      numero,
+      cidade,
+      coordenada,
+      horarioInicio,
+      horarioFim,
+      descricao,
+      imagem,
+      userId,
+      turno,
+    },
   });
 
   if (tags.length > 0) {
@@ -329,59 +403,79 @@ router.post("/", async (req, res) => {
     }
   }
 
-    if (diaSemana.length > 0) {
+  const categorias = await prisma.categoria.findFirst({
+    where: { id: categoria },
+  });
 
-      for (const dia of diaSemana) {
-        const diaSemana = await prisma.diaSemana.findFirst({
-          where: { nome: dia },
-        });
-        if (!diaSemana) {
-          return res.status(404).json({ message: `Dia da semana ${dia} não encontrado` });
-        }
-        const diaSemanaFeira = await prisma.diaSemanaFeira.create({
-          data: {
-            diaSemanaId: diaSemana.id,
-            feiraId: feira.id,
-          },
-        });
-      }
-      res.status(201).json(feira);
-    } else {
-      const feiraDia = await prisma.feira.update({
-        where: { id: feira.id },
-        data: { data: new Date(data) },
-      });
+  if (!categorias) {
+    return res.status(404).json({ message: "Categoria não encontrada" });
+  } else {
+    const categoriaFeira = await prisma.categoriaFeira.create({
+      data: {
+        feiraId: feira.id,
+        categoriaId: categorias.id,
+      },
+    });
+  }
 
-      const categorias = await prisma.categoria.findFirst({
-        where: { id: categoria },
+  if (diaSemana.length > 0) {
+    for (const dia of diaSemana) {
+      const diaSemana = await prisma.diaSemana.findFirst({
+        where: { nome: dia },
       });
-      if (!categoria) {
-        return res.status(404).json({ message: "Categoria não encontrada" });
+      if (!diaSemana) {
+        return res
+          .status(404)
+          .json({ message: `Dia da semana ${dia} não encontrado` });
       }
-      
-      if (!categorias) {
-        return res.status(404).json({ message: "Categoria não encontrada" });
-      } else {
-      const categoriaFeira = await prisma.categoriaFeira.create({
+      const diaSemanaFeira = await prisma.diaSemanaFeira.create({
         data: {
+          diaSemanaId: diaSemana.id,
           feiraId: feira.id,
-          categoriaId: categorias.id,
         },
       });
-      }
-      res.status(201).json(feiraDia);
     }
+    res.status(201).json(feira);
+  } else {
+    const feiraDia = await prisma.feira.update({
+      where: { id: feira.id },
+      data: { data: new Date(data) },
+    });
 
-  });
+    res.status(201).json(feiraDia);
+  }
+});
 
 // Update
 router.patch("/:id", async (req, res) => {
   const { id } = req.params;
-  const { nome, endereco, numero, cidade, coordenada, horarioInicio, horarioFim, descricao, imagem, turno } = req.body;
+  const {
+    nome,
+    endereco,
+    numero,
+    cidade,
+    coordenada,
+    horarioInicio,
+    horarioFim,
+    descricao,
+    imagem,
+    turno,
+  } = req.body;
 
   const feira = await prisma.feira.update({
     where: { id: id },
-    data: { nome, endereco, numero, cidade, coordenada, horarioInicio, horarioFim, descricao, imagem, turno },
+    data: {
+      nome,
+      endereco,
+      numero,
+      cidade,
+      coordenada,
+      horarioInicio,
+      horarioFim,
+      descricao,
+      imagem,
+      turno,
+    },
   });
   res.status(200).json(feira);
 });
